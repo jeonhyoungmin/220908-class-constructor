e529c45a884436df35f36e0a409c2fe09f21c4fa
jeonhyoungmin <jhmin7509@gmail.com>
2022-09-08 14:18:19 +0900

    class 생성자 함수

--- index.js
+++ index.js
@@ -16,3 +16,25 @@ class NewConstructor {
 
 const newInstance = new NewConstructor(4, "파이리", "불");
 console.log(newInstance)
+
+const root = document.getElementById('root')
+
+class createTag {
+  constructor(parentTag, ChildTag){
+    this.parentTag = document.createElement(parentTag);
+    this.ChildTag = document.createElement(ChildTag);
+    root.appendChild(this.parentTag)
+    this.parentTag.appendChild(this.ChildTag)
+    this.ChildTag.id = "child"
+  }
+}
+
+const newCreateTag = new createTag('div', 'ul')
+
+console.log(newCreateTag)
+const Child = document.getElementById('child')
+console.log(Child)
+
+console.log(document.body.children[0].children[0].children[0])
+
+console.log(newCreateTag)
